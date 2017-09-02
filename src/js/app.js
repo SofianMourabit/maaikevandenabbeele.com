098759ecc2dd65e6c06296d0227f68179cd69f28
Sofian Mourabit <sofianmourabit@gmail.com>
2017-09-02 18:22:15 +0700

    major site update

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ const mobileMenu = document.querySelector('.mobile__menu');
 const nav = document.querySelector('.nav');
 const body = document.querySelector('.body');
 const header = document.querySelector('.header');
+const logo = document.querySelector('.geen');
 const navContainer = document.querySelector('.nav__container');
 const form = document.querySelector('.form');
 const video = document.getElementById('video');
@@ -22,5 +23,16 @@ navContainer.addEventListener('click', ()=>{
     nav.classList.toggle('is__visible');
     body.classList.toggle('push__left');
     header.classList.toggle('push__left');
+  }
+});
+window.addEventListener('scroll', function() {
+  if(window.scrollY){
+    header.classList.add('is__sticky');
+    navContainer.classList.add('sticky');
+    logo.classList.add('logo__small');
+  }else if(!window.scrollY){
+    header.classList.remove('is__sticky');
+    navContainer.classList.remove('sticky');
+    logo.classList.remove('logo__small');
   }
 });
